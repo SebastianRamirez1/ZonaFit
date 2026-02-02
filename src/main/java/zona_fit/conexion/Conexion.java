37ea99e30296bca3671d01983d36f15ff54adc0b
Sala K 401 <salak401@salasfr.itm.edu.co>
2026-02-02 17:43:24 -0500

    cambios x

--- src/main/java/zona_fit/conexion/Conexion.java
+++ src/main/java/zona_fit/conexion/Conexion.java
@@ -1,4 +1,7 @@
 package zona_fit.conexion;
 
 public class Conexion {
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "localhost";
 }
