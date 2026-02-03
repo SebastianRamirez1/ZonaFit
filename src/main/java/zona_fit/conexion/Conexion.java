c523d4e863e8b3fe1663afcff60849c47a389b1a
Ramitas <sebastianacevedo123.sra@gmail.com>
2026-02-03 18:55:19 -0500

    conexion echa

--- src/main/java/zona_fit/conexion/Conexion.java
+++ src/main/java/zona_fit/conexion/Conexion.java
@@ -1,7 +1,31 @@
 package zona_fit.conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-    private static final String URL = "localhost";
+
+    public static Connection getConnection(){
+        Connection conexion = null;
+        var USER = "root";
+        var PASSWORD = "admin";
+        var BD = "zona_fit_db";
+        var URL = "jdbc:mysql://localhost:3306/" + BD;
+        try{
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch(Exception e){
+            System.out.println("Error al conectar base de datos: " + e.getMessage());
+        }
+        return conexion;
+    }
+
+
+    public static void main(String[] args) {
+        var conexion = Conexion.getConnection();
+        if(conexion != null){
+            System.out.println(conexion);
+        }else{
+            System.out.println("error");
+        }
+    }
 }
